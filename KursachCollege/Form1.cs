8da101df3711e792f08587e51e37d83998d93e43
George Chebodan <gchebodang@gmail.com>
2020-11-20 21:18:53 +0300

    Add project files.

---
+++ KursachCollege/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace KursachCollege
+{
+    public partial class Form1 : Form
+    {
+    private BindingSource bindingSource1 = new BindingSource();
+    private SqlDataAdapter dataAdapter = new SqlDataAdapter();
+    private Button reloadButton = new Button();
+    private Button submitButton = new Button();
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.Dock = DockStyle.Fill;
+            reloadButton.Text = "Reload";
+            submitButton.Text = "Submit";
+            reloadButton.Click += new EventHandler(button1_Click);
+            submitButton.Click += new EventHandler(button2_Click);
+
+            FlowLayoutPanel panel = new FlowLayoutPanel
+            {
+                Dock = DockStyle.Top,
+                AutoSize = true
+            };
+            panel.Controls.AddRange(new Control[] { reloadButton, submitButton });
+
+            Controls.AddRange(new Control[] { dataGridView1, panel });
+            Load += new EventHandler(Form1_Load);
+            Text = "Automatization of bus station";
+        }
+        private void GetData(string selectCommand)
+        {
+            try
+            {
+                // Specify a connection string.
+                String connectionString =
+                    "Data Source= (localdb)\\MSSQLLocalDB;Initial Catalog=KursachCollege;" +
+                    "Integrated Security=True";
+
+                // Create a new data adapter based on the specified query.
+                dataAdapter = new SqlDataAdapter(selectCommand, connectionString);
+
+                // Create a command builder to generate SQL update, insert, and
+                // delete commands based on selectCommand.
+                SqlCommandBuilder commandBuilder = new SqlCommandBuilder(dataAdapter);
+
+                // Populate a new data table and bind it to the BindingSource.
+                DataTable table = new DataTable
+                {
+                    Locale = CultureInfo.InvariantCulture
+                };
+                dataAdapter.Fill(table);
+                bindingSource1.DataSource = table;
+
+                // Resize the DataGridView columns to fit the newly loaded content.
+                dataGridView1.AutoResizeColumns(
+                    DataGridViewAutoSizeColumnsMode.Fill);
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Something went wrong. Pleas, try again. " + ex.ToString());
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Bind the DataGridView to the BindingSource
+            // and load the data from the database.
+            dataGridView1.DataSource = bindingSource1;
+            GetData("select * from Names");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Reload the data from the database.
+            GetData(dataAdapter.SelectCommand.CommandText);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Update the database with changes.
+            dataAdapter.Update((DataTable)bindingSource1.DataSource);
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ KursachCollege/Registration.cs
@@ -0,0 +1,100 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Text;
+using System.Windows.Forms;
+
+namespace KursachCollege    
+{
+    public partial class Registration : Form
+    {
+       // private MySqlDataAdapter dataAdapter = new MySqlDataAdapter();
+        public Registration()
+        {
+            InitializeComponent();
+        }
+        static string connectionString =
+                "Data Source= (localdb)\\MSSQLLocalDB;Initial Catalog=KursachCollege;" +
+                "Integrated Security=True";
+        public MySqlConnection con = new MySqlConnection(connectionString);
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            MySqlCommand command = con.CreateCommand();
+            command.CommandType = CommandType.Text;
+            command.CommandText ="select * from Users where username = '" + textBox1.Text+"'and password'" + textBox2.Text + "'";
+            DataTable dt = new DataTable();
+            MySqlDataAdapter da = new MySqlDataAdapter(command);
+            da.Fill(dt);
+            i = Convert.ToInt32(dt.Rows.Count.ToString());
+            if (i == 0)
+            {
+                if (radioButton1.Checked)
+                {
+                    InsertAuto(textBox1.Text, textBox2.Text);
+                }
+                else
+                {
+                    MessageBox.Show("Неправильно указано имя пользователя или пароль");
+                }
+            }
+            else
+            {
+               // this.Hide();
+              //  Form1 form = new Form1();
+               // form.Show();
+            }
+        }
+        public void InsertAuto(string username, string password)
+        {
+            con.Open();
+            // Оператор SQL
+            string sql = string.Format("Insert Into Users" +
+                   "(username, password) Values(@username, @password)");
+
+            using (MySqlCommand cmd = new MySqlCommand(sql, this.con))
+            {
+                // Добавить параметры
+                cmd.Parameters.AddWithValue("@username", username);
+                cmd.Parameters.AddWithValue("@password", password);
+                cmd.ExecuteNonQuery();
+            }
+        }
+        private void Registration_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+            {
+                radioButton2.Checked = false;
+                radioButton1.Checked = true;
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                radioButton1.Checked = false;
+                radioButton2.Checked = true;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
